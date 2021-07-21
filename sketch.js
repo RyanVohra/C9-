412e1cdd1b98e60350640fc094ba97f5c5f2f9fd
RyanVohra <87702860+RyanVohra@users.noreply.github.com>
2021-07-21 17:06:07 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+  background(30);
+if(keyDown("space")){
+background("red")
+}
+if(keyDown("DOWN_ARROW")){
+  background("blue")
+  }
+  if(keyDown("UP_ARROW")){
+    background("orange")
+    }
+    if(keyDown("LEFT_ARROW")){
+      background("green")
+      }
+
+
+
+
+drawSprites()
+
+}
+
+
+
+
